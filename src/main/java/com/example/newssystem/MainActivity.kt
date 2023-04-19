a13f41c5a306200b065d2ce336c805ce3f30c964
CHANGNEWS <129360089+CHANGNEWS@users.noreply.github.com>
2023-04-19 16:10:06 +0530

    Update MainActivity.kt

--- src/main/java/com/example/newssystem/MainActivity.kt
+++ src/main/java/com/example/newssystem/MainActivity.kt
@@ -1,40 +1,131 @@
-package com.example.newssystem
+package com.example.newsapp
 
+
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.newssystem.ui.theme.NewsSystemTheme
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            NewsSystemTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Google Sign Option
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+
+        // Shared Preference Code
+        val pref = getSharedPreferences("login", MODE_PRIVATE)
+        val check = pref.getBoolean("flag", false)
+        if (check) { // for true Login (User already login )
+            startActivity(Intent(this@MainActivity, NewsActivity::class.java))
+            finish()
+        }
+
+
+        //Google Sign
+        google_sign_btn.setOnClickListener {
+            signInGoogle()
+        }
+
+        // Sign with Email I'd and Password
+        sign_in_btn.setOnClickListener {
+            val email = edt_email.text.toString()
+            val password = edt_password.text.toString()
+            if (email.isEmpty()) edt_email.error = "Please enter the email"
+            if (password.isEmpty()) edt_password.error = "Please enter the password"
+            else {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        // Shared Preference
+                        val pref = getSharedPreferences("login", MODE_PRIVATE)
+                        val editor = pref.edit()
+                        editor.putBoolean("flag", true)
+                        editor.apply()
+                        startActivity(Intent(this, NewsActivity::class.java))
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
+            edt_email.text = null
+            edt_password.text = null
+        }
+
+        register.setOnClickListener {
+            startActivity(Intent(this,Sign_up::class.java))
+            finish()
+        }
+
+        sign_up.setOnClickListener {
+            startActivity(Intent(this,Sign_up::class.java))
+            finish()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NewsSystemTheme {
-        Greeting("Android")
+    private fun signInGoogle() {
+        val signInIntent = this.googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
     }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+                if(result.resultCode == Activity.RESULT_OK){
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    handleResults(task)
+                }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this,task.exception.toString(),Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener{
+            if (it.isSuccessful){
+                // Shared Preference
+                val pref = getSharedPreferences("login", MODE_PRIVATE)
+                val editor = pref.edit()
+                editor.putBoolean("flag", true)
+                editor.apply()
+                startActivity(Intent(this, NewsActivity::class.java))
+                finish()
+            }
+            else{
+                Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
 }
